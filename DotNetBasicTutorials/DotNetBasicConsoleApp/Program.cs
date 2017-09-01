5971d373749c431c4bd6fb9e0be3a879f6c71dab
唯手熟尔 <duhongle@outlook.com>
2017-09-01 16:39:59 +0800

    添加项目文件。

---
+++ DotNetBasicTutorials/DotNetBasicConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DotNetBasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sw = Stopwatch.StartNew();
+            sw.Reset();
+            var ss = new SortedSet<int>(Enumerable.Repeat(42, 400_000));
+            Console.WriteLine(sw.Elapsed);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DotNetCoreTutorials/DotNetCoreConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace DotNetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Get(512));
+
+            var sw = Stopwatch.StartNew();
+            while (true)
+            {
+                sw.Reset();
+                sw.Start();
+
+                var ss = new SortedSet<int>(Enumerable.Repeat(42, 400_000));
+
+                var q = new Queue<int>();
+                for (int i = 0; i < 200_000_000; i++)
+                {
+                    q.Enqueue(i);
+                    q.Dequeue();
+                }
+
+                var result = 0;
+                var s = new SortedSet<int>();
+                for (int i = 0; i < 100_000; i++)
+                {
+                    s.Add(i);
+                }
+                for (int i = 0; i < 100_000_000; i++)
+                {
+                    result = s.Min;
+                }
+
+                var list = new List<int>();
+                for (int i = 0; i < 100_100_100; i++)
+                {
+                    list.Add(i);
+                    list.RemoveAt(0);
+                }
+
+                Console.WriteLine(sw.Elapsed);
+            }
+        }
+
+        public static int Get(int number) => Newtonsoft.Json.JsonConvert.DeserializeObject<int>($"{1000 + number}");
+    }
+}
